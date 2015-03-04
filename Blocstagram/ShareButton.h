6251ac1954c4fc5b6bb532285159c62f7b9c697e
Julicia James <d.james128@gmail.com>
2015-03-03 22:15:01 -0500

    Share button

---
+++ Blocstagram/ShareButton.h
@@ -0,0 +1,13 @@
+//
+//  ShareButton.h
+//  Blocstagram
+//
+//  Created by Julicia on 2/26/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShareButton : NSObject
+
+@end
---
+++ Blocstagram/ShareButton.m
@@ -0,0 +1,13 @@
+//
+//  ShareButton.m
+//  Blocstagram
+//
+//  Created by Julicia on 2/26/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ShareButton.h"
+
+@implementation ShareButton
+
+@end
